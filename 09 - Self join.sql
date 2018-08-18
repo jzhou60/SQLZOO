8db3ddb58cfe72f1cabe20a119e37f950c3d1ed3
jzhou60 <ryan.zhoujt@gmail.com>
2018-08-17 22:25:09 -0500

    Update 09 - Self join.sql

--- 09 - Self join.sql
+++ 09 - Self join.sql
@@ -72,3 +72,13 @@ FROM route a
   JOIN stops stopa ON (a.stop=stopa.id) 
   JOIN stops stopb ON (b.stop=stopb.id) 
 WHERE stopa.name = 'Craiglockhart' AND stopb.name = 'Tollcross'
+
+/*9.
+Give a distinct list of the stops which may be reached from 'Craiglockhart' by taking one bus, including 'Craiglockhart' itself, offered by the LRT company. Include the company and bus no. of the relevant services.*/
+
+SELECT stopa.name, a.company, a.num 
+FROM route a 
+  JOIN route b ON (a.num=b.num AND a.company=b.company) 
+  JOIN stops stopa ON (a.stop=stopa.id) 
+  JOIN stops stopb ON (b.stop=stopb.id) 
+WHERE stopb.name = 'Craiglockhart'
